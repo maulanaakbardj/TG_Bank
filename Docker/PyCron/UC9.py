77d25df514339ded2b3f277d9aabe29851cdc1ab
Maulana Akbar Dwijaya <67249292+maulanaakbardj@users.noreply.github.com>
2023-01-05 14:14:01 +0700

    Update UC9.py

--- Docker/PyCron/UC9.py
+++ Docker/PyCron/UC9.py
@@ -1,6 +1,13 @@
-import pyTigerGraph as tg 
-conn = tg.TigerGraphConnection(host="http://192.168.100.8")
+import pyTigerGraph as tg
+import pytz
+from datetime import datetime
+
+now = datetime.now()
+localtz = pytz.timezone('Asia/Jakarta')
+date_aware_la = localtz.localize(now).strftime("%Y-%m-%d %H:%M:%S")
+
+conn = tg.TigerGraphConnection(host="http://192.168.100.8") # Maul Ip
 conn.graphname = "Graph_Practice_New"
 runquery = conn.runInstalledQuery("UC9_2Vertex")
-parsR = (runquery)
-print(parsR)
+
+print("successful date:", date_aware_la)
