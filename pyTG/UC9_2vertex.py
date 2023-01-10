95e623b8bfba5c930a30084d46584f8c10334ca6
Maulana Akbar Dwijaya <67249292+maulanaakbardj@users.noreply.github.com>
2023-01-10 17:45:17 +0700

    Delete UC9_2vertex.py

--- pyTG/UC9_2vertex.py
+++
@@ -1,6 +0,0 @@
-import pyTigerGraph as tg 
-conn = tg.TigerGraphConnection(host="http://localhost")
-conn.graphname = "Graph_Practice_New"
-runquery = conn.runInstalledQuery("UC9_2Vertex")
-parsR = (runquery)
-print(parsR)
